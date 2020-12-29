65a842ccd4626cbd8a7be2593d0e64bc2afd2c4a
dianyuluo <1739556970@qq.com>
2020-12-29 21:49:38 +0800

    over a list

---
+++ c++/CLION/myList.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 点雨落山岚 on 2020/12/29.
+//
+#include "iostream"
+#include "algorithm"
+
+template <typename Object>
+class List{
+private:
+    struct Node{
+
+    };
+public:
+    class const_iterator{
+
+    };
+    class iterator:public const_iterator{
+
+    };
+};
+
+int main(){
+
+}
+
