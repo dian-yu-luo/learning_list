8ef689d5db7dfcf687678c58f8bb2b19f2b2427f
dianyuluo <1739556970@qq.com>
2020-12-29 18:08:36 +0800

    no name

---
+++ c++/CLION/Nlist.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 点雨落山岚 on 2020/12/29.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    int t1=3;
+    int t2;
+    t2 = move(t1);
+    cout<<t2;
+    cout<<endl<<t1;
+}
