bd4cc3a952b53040e541d3b4c04a95cb5482658b
dianyuluo <1739556970@qq.com>
2020-12-30 21:15:10 +0800

    zereo

--- README.md
+++
@@ -1,3 +0,0 @@
-# vacation_learning_list
-to remeber what I have learned
-
--- c++/CLION/temp store/about_vector.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by 点雨落山岚 on 2020/12/25.
-//
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"liang q"<<endl;
-}
