dd54066305df8349bae40ba1233e86af2e57ebed
dianyuluo <1739556970@qq.com>
2021-05-25 14:16:50 +0800

    java and mysql

---
+++ language/c++/workasaBee/BinaryHeap.cpp
@@ -0,0 +1,83 @@
+﻿#include "iostream"
+#include "vector"
+
+using namespace std;
+
+class UnderflowException {
+};
+
+template<typename Comparable>
+class BinaryHeap {
+public:
+    explicit BinaryHeap(int capacity = 100);
+
+    explicit BinaryHeap(const vector<Comparable> &items);
+
+    bool isEmpty() const;
+
+    const Comparable &findMin() const;
+
+    void insert(const Comparable &x) {
+        if (currentSize == array.size() - 1) {
+            array.resize(array.size() * 2);
+        }
+        int hole = ++currentSize;
+        Comparable copy = x;
+        //为了不去直接操作x
+        array[0] = move(copy);
+        for (; x < array[hole / 2]; hole /= 2) {
+            array[hole] = move(array[hole / 2]);
+        }
+        array[hole] = move(array[0]);
+        //array[0]的值没有被重复置零，但是被改变下一次被调用是被改变归一
+
+    };
+
+    void insert(Comparable &&x);
+
+    void deleteMin() {
+        if (isEmpty())
+            throw UnderflowException{};
+        array[1] = move(array[currentSize--]);
+        percolateDown(1);
+    };
+
+    void deleteMin(Comparable &minItem){
+        if (isEmpty())
+            throw UnderflowException{};
+        minItem = move(array[1]);
+        array[1]= move(array[currentSize--]);
+        percolateDown(1);
+    };
+
+    void makeEmpty();
+
+private:
+    int currentSize;
+
+    vector<Comparable> array;
+//    表示把所有的元素存在一个实例中
+
+    void buildHeap();
+
+    void percolateDown(int hole) {
+        int child;
+        Comparable tmp = move(array[hole]);
+        for (; hole * 2 <= currentSize; hole = child) {
+            child = hole * 2;
+            if (child != currentSize && array[child + 1] < array[child]) {
+                ++child;
+            }
+            if (array[child] < tmp) {
+                array[hole] = move(array[child]);
+            } else
+                break;
+        }
+    };
+
+};
+
+int main() {
+
+
+}
